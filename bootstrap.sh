ffb054fa137f763ac6f685272fe4a1e7321989dc
karan <karan.sharma@heavywater.solutions>
2019-01-17 11:23:28 -0500

    Less Confusing Script CheckingResponseHeader

--- bootstrap.sh
+++ bootstrap.sh
@@ -23,29 +23,14 @@ fi
 read -r -p "Enter your GitHub UserName: " HW_GITHUB_USER
 git config --global github.username "$HW_GITHUB_USER"
 
-# myvar=$(curl -u $HW_GITHUB_USER https://api.github.com/user | grep message)
-# IFS=':' read -r -a array <<< "$myvar"
-# mymsg=${array[1]}
-# l=${#mymsg}
-
-# # RESPONSE_FROM_API/valueOf(mymsg) = `"Must specify two-factor authentication OTP code.",`
-# # NOTE: The above response has length of 52 - we are taking a substring(2,49) to match with the desired response
-
-# mymsg=$(echo $mymsg | cut -c2-49)   
-# if [ "$mymsg" = "Must specify two-factor authentication OTP code." ]; then
-#     echo 2FA setup verified, resuming script execution.
-# else
-#     echo Setup MFA or Check credentials.
-#     exit 1
-# fi
-
-resp=$(curl -u "$HW_GITHUB_USER:$password" https://api.github.com/user 2> /dev/null)
-if grep "Must specify two-factor authentication OTP code."; then
-  echo "Setup MFA or Check credentials."
+resp=$(curl -i -u $HW_GITHUB_USER https://api.github.com/user | grep "X-GitHub-OTP: required")
+if [ "${#resp}" = "0" ]; then        
+  echo Setup MFA or Check credentials
   exit 1
 else
-  echo "2FA setup verified, resuming script execution."
+  echo MFA setup verified, resuming script execution
 fi
+
 
 read -r -p "Enter your heavywater.com email: " HW_EMAIL
 git config --global user.email "$HW_EMAIL"
